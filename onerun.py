58f009810b4a2b54bcd95d2c8c518f7a00a225eb
Rachel Qiu <rachel@Rachels-iMac.home>
2020-06-18 17:52:15 -0700

    ss

--- onerun.py
+++ onerun.py
@@ -4,7 +4,6 @@ import requests
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.opera import OperaDriverManager
 import sys
 import os
 import json
--- setup.md
+++ setup.md
@@ -15,3 +15,11 @@ a = ua.random
 user_agent = ua.random
 print(user_agent)
 
+
+
+server side 
+sudo apt install python3.7
+python3.7 -m pip install pip
+
+python3.7 -m pip install <module>
+pip install --upgrade pip
